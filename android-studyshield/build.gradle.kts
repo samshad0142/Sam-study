4142953e0fcd52302089af2b16d418fbc3f6cf79
samshad0142 <samshad0142@gmail.com>
2026-08-27 03:09:49 +0530

    Add files via upload

--- android-studyshield/build.gradle.kts
+++ android-studyshield/build.gradle.kts
@@ -1,4 +1,7 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+android { namespace="com.samstudy.studyshield"; compileSdk=35
+    defaultConfig { applicationId="com.samstudy.studyshield"; minSdk=26; targetSdk=35; versionCode=1; versionName="1.0.0" }
 }
